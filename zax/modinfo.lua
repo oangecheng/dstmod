f45a80db08dba37a87d4dc218d1ad2cd20a5a515
chengzhi <chengzhi@kuaishou.com>
2022-01-18 19:04:05 +0800

    zax modeinfo

---
+++ zax/modinfo.lua
@@ -0,0 +1,24 @@
+-- 名称
+name = "zaxtest"
+-- 描述
+description = "Zax Test Mod"
+-- 作者
+author = "orange"
+-- 版本
+version = "0.1"
+-- klei官方论坛地址，为空则默认是工坊的地址
+forumthread = ""
+-- modicon 下一篇再介绍怎么创建的
+-- icon_atlas = "images/modicon.xml"
+-- icon = "images/modicon.tex"
+-- dst兼容
+dst_compatible = true
+-- 是否是客户端mod
+client_only_mod = false
+-- 是否是所有客户端都需要安装
+all_clients_require_mod = true
+-- 饥荒api版本，固定填10
+api_version = 10
+
+-- mod的配置项，后面介绍
+configuration_options = {}

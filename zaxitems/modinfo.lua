6ae139d9f10b60942c0e62a0da2803a4c32ff3c7
unknown <281725011@qq.com>
2022-02-02 23:46:48 +0800

    zaxitems

--- zaxitems/modinfo.lua
+++ zaxitems/modinfo.lua
@@ -5,12 +5,14 @@ description = "LevelUp!"
 -- 作者
 author = "Zax"
 -- 版本
-version = "0.1"
+version = "0.2"
 -- klei官方论坛地址，为空则默认是工坊的地址
 forumthread = ""
 -- modicon 下一篇再介绍怎么创建的
 -- icon_atlas = "images/modicon.xml"
 -- icon = "images/modicon.tex"
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
 -- dst兼容
 dst_compatible = true
 -- 是否是客户端mod
--- zaxitems/modmain.lua
+++ zaxitems/modmain.lua
@@ -11,17 +11,12 @@ require 'strings_zaxitems_c'
 
 PrefabFiles = {
 	"kj",
-	"lingdongamulet",	
 }
 
  Assets = {
  	-- 合金铠甲
 	Asset("ATLAS", "images/inventoryimages/kj.xml"),
 	Asset("IMAGE", "images/inventoryimages/kj.tex"),
-
-	-- 凛冬护符
-	Asset( "ATLAS", "images/inventoryimages/lingdongamulet.xml"),
-	Asset( "IMAGE", "images/inventoryimages/lingdongamulet.tex"),
 }
 
 
@@ -29,9 +24,3 @@ PrefabFiles = {
 AddRecipe("kj",{GLOBAL.Ingredient("armorwood", 1), GLOBAL.Ingredient("armor_sanity", 1),GLOBAL.Ingredient("armormarble", 1), GLOBAL.Ingredient("armorruins", 1),GLOBAL.Ingredient("raincoat", 1)},
 RECIPETABS.SURVIVAL, TECH.NONE, nil, nil, nil, nil, nil, 
 "images/inventoryimages/kj.xml", "kj.tex")
-
-
--- 生命护符x1 + 寒冬背心x1 + 龙鳞x1 + 橙色宝石x1
-AddRecipe("lingdongamulet", {Ingredient("amulet", 1), Ingredient("trunkvest_winter", 1),Ingredient("dragon_scales", 1),Ingredient("orangegem", 1)}, 
-RECIPETABS.MAGIC, TECH.MAGIC_TWO, nil, nil, nil, nil, nil, "images/inventoryimages/lingdongamulet.xml", "lingdongamulet.tex")
-
